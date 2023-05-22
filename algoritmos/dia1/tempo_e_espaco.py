b8e0f3ffce1af54fb9cf81e5474d018f3a015a48
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-05-22 10:08:33 -0300

    .

---
+++ algoritmos/dia1/tempo_e_espaco.py
@@ -0,0 +1,11 @@
+def multiply_array(numbers):
+    result = 1
+    for number in numbers:
+        result *= number
+
+    return result
+
+
+# Complexidade de Tempo: O(n)
+
+# Complexidade de Espaço: O(1)
